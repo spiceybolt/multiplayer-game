f66051afe7b78d7d8efe82cae58dd43ce798240a
spiceybolt <a>
2021-04-20 21:28:08 +0530

    documented and fixed client colour assignment bug

--- player.py
+++ player.py
@@ -5,19 +5,22 @@ import json
 
 pg.init()
 
+
 class Game():
 	def __init__(self, address):
+		#init socket, screen and player and server rects, colours and physics
 		self.sock = socket.socket()
 
 		self.sock.connect(address)
 		self.c = self.sock.recv(1024).decode()
+		print(self.c)
 
 		self.size = (800,600)
 		self.screen = pg.display.set_mode(self.size)
 		pg.display.set_caption("Multiplayer python game, bi-sh")
 
-		self.playerrect = pg.Rect(0, 0, 20, 20)
-		self.screenrect = self.screen.get_rect()
+		self.player_rect = pg.Rect(0, 0, 20, 20)
+		self.screen_rect = self.screen.get_rect()
 
 		self.colours = {'r':(255,0,0),'g':(0,255,0),'b':(0,0,255)}
 		self.velX = 0
@@ -25,7 +28,7 @@ class Game():
 		self.speed = 2
 
 
-	def handleInput(self):
+	def handle_input(self):
 		for event in pg.event.get():
 			if event.type == pg.QUIT:
 				self.quit()
@@ -51,15 +54,14 @@ class Game():
 			
 		while True:
 			#getting input from user
-			self.handleInput()
+			self.handle_input()
 
 			#moving the player and confining within screen
-			self.playerrect.move_ip(self.velX, self.velY)
-			self.playerrect.clamp_ip(self.screenrect)
+			self.player_rect.move_ip(self.velX, self.velY)
+			self.player_rect.clamp_ip(self.screen_rect)
 
 			#sending the server player's position
-			print((self.playerrect.x, self.playerrect.y, self.c))
-			self.sock.send(pickle.dumps((self.playerrect.x, self.playerrect.y, self.c)))
+			self.sock.send(pickle.dumps((self.player_rect.x, self.player_rect.y, self.c)))
 
 			#get all player positions from server
 			playerdata = pickle.loads(self.sock.recv(1024))
@@ -70,7 +72,7 @@ class Game():
 				if not i[-1] == self.c:
 					pg.draw.rect(self.screen, self.colours[i[-1]], pg.Rect(i[0],i[1],20,20))
 
-			pg.draw.rect(self.screen, self.colours[self.c], self.playerrect)
+			pg.draw.rect(self.screen, self.colours[self.c], self.player_rect)
 
 			pg.display.flip()
 
--- server.py
+++ server.py
@@ -4,52 +4,78 @@ import pickle
 import json
 
 playerdata = []
-playerdatalock = threading.Lock()
+playerdata_lock = threading.Lock()
 
+#modify lock logic pls
 
 count = 0
-countlock = threading.Lock()
+count_lock = threading.Lock()
 
 colours = ['r','g','b']
-colourlock = threading.Lock()
+colours_lock = threading.Lock()
 
-def playerthread(client, addr, name:str):
+def player_thread(client, addr, name:str):
 	global count
 
-	countlock.acquire()
-	colourlock.acquire()
+	count_lock.acquire()
+	colours_lock.acquire()
+	playerdata_lock.acquire()
 	# assigning colours to dudes in the order they joined(needs work later)
-	client.send(colours[count - 1].encode())
-	thiscolour = colours[count - 1]
-	countlock.release()
-	colourlock.release()
+	print(f"{threading.active_count()} is number of threads active rn")
+	#assigning unassigned colour to new client
+	for i in colours:
+		if i not in [j[-1] for j in playerdata]:
+			client.send(i.encode())
+			thiscolour = i
+			print(i)
+			break
+	count_lock.release()
+	playerdata_lock.release()
+	colours_lock.release()
 	
+
+	#client loop
 	while True:
+		#getting message from client
 		msg = client.recv(1024)
+
+		#quit if msg == b'' its the quit message
 		if msg == b'':
 			break
-		pickleobj = pickle.loads(msg)
 
-		countlock.acquire()
-		playerdatalock.acquire()
-		if not pickleobj[-1] in [j[-1] for j in playerdata]:
-			playerdata.append(pickleobj)
+		#if it isnt a " b'' " then it must be a pickle obj. pickle-it
+		pickle_obj = pickle.loads(msg)
+
+		count_lock.acquire()
+		playerdata_lock.acquire()
+
+		#if this is the first time this loop is running, then there is no 
+		#data of this colour in the playerdata 
+		#so if the colour aint in playerdata append this obj(tuple)
+		#to our playerdata list
+		if not pickle_obj[-1] in [j[-1] for j in playerdata]:
+			playerdata.append(pickle_obj)
+		#else playerdata[function to get index of the data we need to update] = obj
+		# a try to catch an error ive been getting to find the cause
 		else:
-			playerdata[playerdata.index(next(i for i in playerdata if pickleobj[-1] in i))] = pickle.loads(msg)
-		print(playerdata)
+			try:
+				playerdata[playerdata.index(next(i for i in playerdata if pickle_obj[-1] in i))] = pickle_obj
+			except StopIteration:
+				print(playerdata)
+		#sending board info to client
 		client.send(pickle.dumps(playerdata))
-		playerdatalock.release()
-		countlock.release()
+
 		
-		# client.send(b'got')
+		playerdata_lock.release()
+		count_lock.release()
+	
+	#closing thread after b''->break was called	
 	print("closed client")
+	#removing the data of player from playerdata
 	playerdata.pop(playerdata.index(next(i for i in playerdata if thiscolour in i)))
 	
-	countlock.acquire()
-	count -= 1
-	countlock.release()
 
-
+#opening file and init socket
 with open('config.json','r') as f:
 	data = json.load(f)
 
@@ -61,9 +87,11 @@ print(server.getsockname())
 
 server.listen(5)
 
+#mainloop listens to connections and starts thread(need to remove count)
 while True:
 	client, addr = server.accept()
 	count += 1
-	print(client)
-	threading.Thread(target=playerthread, args=(client, addr, f'player {count}',)).start()
+	print(f"player {threading.active_count()} has connected")
+	threading.Thread(target=player_thread, args=(client, addr, f'player {threading.active_count()}',)).start()
+	print(threading.active_count())
 
