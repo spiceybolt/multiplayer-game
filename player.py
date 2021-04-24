9a6704698928d9243e0b2dece72bc014557ee6d8
spiceybolt <a>
2021-04-24 21:11:04 +0530

    added  separate classes for players and drew sprite

--- player.py
+++ player.py
@@ -6,6 +6,96 @@ import json
 
 pg.init()
 
+class Client(pg.sprite.Sprite):
+	def __init__(self, color_name, color, image_list):
+		pg.sprite.Sprite.__init__(self)
+
+		self.images = image_list
+
+		self.color_name = color_name
+		self.color = pg.Color(*color)
+
+
+		self.rect = pg.Rect(0, 0, self.images[0].get_width(), self.images[0].get_height())
+
+
+	def update(self, playerdata):
+		for i in playerdata:
+			if i[-1] == self.color_name:
+				self.rect.x = i[0]
+				self.rect.y = i[1]
+				break
+		#if didnt break, means no data for this poor dude
+		else:
+			self.kill()
+
+	def draw(self, surface):
+		surface.blit(self.images[0], self.rect)
+		pg.draw.polygon(surface, self.color, [(self.rect.x + self.rect.w/2, self.rect.y),
+											  (self.rect.x + self.rect.w/4, self.rect.y - self.rect.h/4),
+											  (self.rect.x + 3*self.rect.w/4, self.rect.y - self.rect.h/4)])
+
+
+
+
+class Player(pg.sprite.Sprite):
+	def __init__(self, bounds,color_name, color, image_list):
+		# super(Player, self).__init__()
+		pg.sprite.Sprite.__init__(self)
+		self.images = image_list
+
+		# img = pg.image.load("res/player.png")
+		self.color = pg.Color(*color)
+		self.color_name = color_name
+	
+		self.rect = self.images[0].get_rect()
+		self.dir = [0,0]
+		self.pos_x = 0
+		self.pos_y = 0
+		self.speed = 800
+
+		#player wont go past this rectangle bounds
+		self.bounds = bounds
+
+
+	def set_velocity_x(self,x:int):
+		if x > 1 or x < -1 :
+			print("only -1,0,1")
+			return
+		self.dir[0] = x
+
+	def set_velocity_y(self, y:int):
+		if y > 1 or y < -1:
+			print("only -1,0,1")
+			return
+		self.dir[1] = y
+
+
+	def update(self, dt:float):
+		self.pos_x += self.speed*self.dir[0]*dt
+		self.pos_y += self.speed*self.dir[1]*dt
+
+		if self.pos_x + self.rect.w > self.bounds[0]:
+			self.pos_x = self.bounds[0] - self.rect.w
+		elif self.pos_x < 0 : 
+			self.pos_x = 0
+		if self.pos_y + self.rect.h > self.bounds[1]:
+			self.pos_y = self.bounds[1] - self.rect.h
+		elif self.pos_y < 0:
+			self.pos_y = 0
+
+
+		self.rect.x = self.pos_x
+		self.rect.y = self.pos_y
+
+
+	def draw(self, surface):
+		surface.blit(self.images[0], self.rect)
+		pg.draw.polygon(surface, self.color, [(self.rect.x + self.rect.w/2, self.rect.y),
+											  (self.rect.x + self.rect.w/4, self.rect.y - self.rect.h/4),
+											  (self.rect.x + 3*self.rect.w/4, self.rect.y - self.rect.h/4)])
+
+
 
 class Game():
 	def __init__(self, address):
@@ -15,20 +105,19 @@ class Game():
 		self.sock.connect(address)
 		self.c = self.sock.recv(1024).decode()
 
+		self.client_count = 0
+
 		self.size = (800,600)
 		self.screen = pg.display.set_mode(self.size)
 		pg.display.set_caption("Multiplayer python game, bi-sh")
 
-		self.player_rect = pg.Rect(0, 0, 20, 20)
-		self.screen_rect = self.screen.get_rect()
+		self.person_images = []
+		self.person_images.append(pg.image.load("res/player.png"))
 
 		self.colours = {'r':(255,0,0),'g':(0,255,0),'b':(0,0,255)}
-		self.velX = 0
-		self.velY = 0
-		self.pos_x = 0
-		self.pos_y = 0
-		self.speed = 40
 
+		self.player = Player(self.size, self.c, self.colours[self.c], self.person_images)
+		self.clients = pg.sprite.Group()
 		self.clock = pg.time.Clock()
 
 
@@ -36,23 +125,24 @@ class Game():
 		for event in pg.event.get():
 			if event.type == pg.QUIT:
 				self.quit()
+
 			elif event.type == pg.KEYDOWN:
 				if event.key == pg.K_ESCAPE:
 					self.quit()
 				elif event.key == pg.K_UP:
-					self.velY = -self.speed
+					self.player.set_velocity_y(-1)
 				elif event.key == pg.K_DOWN:
-					self.velY = self.speed
+					self.player.set_velocity_y(1)
 				elif event.key == pg.K_LEFT:
-					self.velX = -self.speed
+					self.player.set_velocity_x(-1)
 				elif event.key == pg.K_RIGHT:
-					self.velX = self.speed
+					self.player.set_velocity_x(1)
+			
 			elif event.type == pg.KEYUP:
 				if event.key == pg.K_UP or event.key == pg.K_DOWN:
-					self.velY = 0
+					self.player.set_velocity_y(0)
 				elif event.key == pg.K_LEFT or event.key == pg.K_RIGHT:
-					self.velX = 0
-
+					self.player.set_velocity_x(0)
 
 	def mainloop(self):	
 		while True:
@@ -62,27 +152,33 @@ class Game():
 			#moving the player and confining within screen
 			dt = self.clock.tick()/1000.0
 
-			
-			self.pos_x += self.velX*dt
-			self.pos_y += self.velY*dt
-			self.player_rect.x = self.pos_x
-			self.player_rect.y = self.pos_y
-			
-			self.player_rect.clamp_ip(self.screen_rect)
+			self.player.update(dt)
 
 			#sending the server player's position
-			self.sock.send(pickle.dumps((self.player_rect.x, self.player_rect.y, self.c)))
+			self.sock.send(pickle.dumps((self.player.pos_x, self.player.pos_y, self.c)))
 
 			#get all player positions from server
 			playerdata = pickle.loads(self.sock.recv(1024))
 
+			
 			#clear screen, draw player and show screen
-			self.screen.fill((0,0,0))
-			for i in playerdata:
-				if not i[-1] == self.c:
-					pg.draw.rect(self.screen, self.colours[i[-1]], pg.Rect(i[0],i[1],20,20))
+			self.screen.fill((255,255,255))
 
-			pg.draw.rect(self.screen, self.colours[self.c], self.player_rect)
+			self.player.draw(self.screen)
+
+			if len(playerdata) > self.client_count + 1 :
+				#new dude has joined the connection..
+				for each in playerdata:
+					if each[-1] != self.player.color_name and each[-1] not in [i.color_name for i in self.clients.sprites()]:
+						self.clients.add(Client(each[-1], self.colours[each[-1]], self.person_images))
+						print(each[-1])
+						self.client_count += 1
+
+					print(each)
+
+			self.clients.update(playerdata)
+			for i in self.clients.sprites():
+				i.draw(self.screen)
 
 			pg.display.flip()
 
